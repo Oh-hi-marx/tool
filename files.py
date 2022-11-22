e400452e6bd3f3611df5869ac201d44951ec8a6d
oh-hi-marx <flabdula@gmail.com>
2022-11-22 14:25:33 +1300

    auto commit -init

---
+++ files.py
@@ -0,0 +1,10 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
+def onlyfiles(mypath):
+	onlyfiles = [join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f))]
+	return onlyfiles
+	
+def onlyfolders(mypath):
+	return next(os.walk('.'))[1]
