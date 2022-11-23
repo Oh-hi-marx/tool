a66a40b94674356c51e70cef47fd625d8c8f60ce
oh-hi-marx <flabdula@gmail.com>
2022-11-23 15:57:14 +1300

    auto commit -s

--- videos.py
+++ videos.py
@@ -2,21 +2,21 @@
 import os
 import cv2
 import numpy as np
-def letterbox_image(image, expected_size):
-    ih, iw, _ = image.shape
-    eh, ew = expected_size
-    scale = min(eh / ih, ew / iw)
-    nh = int(ih * scale)
-    nw = int(iw * scale)
-
-    image = cv2.resize(image, (nw, nh), interpolation=cv2.INTER_CUBIC)
-    new_img = np.full((eh, ew, 3), 128, dtype='uint8')
-    # fill new image with the resized image and centered it
-    new_img[(eh - nh) // 2:(eh - nh) // 2 + nh,
-            (ew - nw) // 2:(ew - nw) // 2 + nw,
-            :] = image.copy()
-    return new_img
-def extractFrames(inpath, outpath, resolution = (1080,1920)):
+def letterbox_image(image, size):
+    '''resize image with unchanged aspect ratio using padding'''
+    iw, ih = image.shape[0:2][::-1]
+    w, h = size
+    scale = min(w/iw, h/ih)
+    nw = int(iw*scale)
+    nh = int(ih*scale)
+    image = cv2.resize(image, (nw,nh), interpolation=cv2.INTER_CUBIC)
+    new_image = np.zeros((size[1], size[0], 3), np.uint8)
+    new_image.fill(128)
+    dx = (w-nw)//2
+    dy = (h-nh)//2
+    new_image[dy:dy+nh, dx:dx+nw,:] = image
+    return new_image
+def extractFrames(inpath, outpath, resolution = (1080,1920), letterBox =0):
 	# Create a VideoCapture object and read from input file
 	# If the input is the camera, pass 0 instead of the video file name
 
@@ -32,13 +32,13 @@ def extractFrames(inpath, outpath, resolution = (1080,1920)):
 	  # Capture frame-by-frame
 	  ret, frame = cap.read()
 	  if ret == True:
-		  if(frame.shape[0:2]!=resolution):
-			  frame = cv2.resize(frame, (resolution[1], resolution[0]))
-		  cv2.imshow('Frame',frame)
+		  if(frame.shape[0:2]!=resolution and letterBox ==1):
+			  frame = letterbox_image(frame, [1920,1080])
+		  #cv2.imshow('Frame',frame)
 		  cv2.imwrite(outpath+ "/" + str(counter)+".jpg", frame)
 		  counter+=1
-		  if cv2.waitKey(25) & 0xFF == ord('q'):
-			  break
+		  #if cv2.waitKey(25) & 0xFF == ord('q'):
+		#	  break
 	  else:
       		break
 	cap.release()
