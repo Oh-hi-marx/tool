e8ce2f5ae7cc2b31cadbcfe59e6e8bb206ea8124
oh-hi-marx <flabdula@gmail.com>
2022-12-15 17:21:12 +1300

    auto commit -

--- test.py
+++ test.py
@@ -2,8 +2,6 @@
 import os
 import cv2
 import numpy as np
-import moviepy.editor as mp
-from files import *
 def letterbox_image(image, size):
     '''resize image with unchanged aspect ratio using padding'''
     iw, ih = image.shape[0:2][::-1]
@@ -45,36 +43,3 @@ def extractFrames(inpath, outpath, resolution = (1080,1920), letterBox =0):
       		break
 	cap.release()
 	cv2.destroyAllWindows()
-
-def renderFromFrames(framesPath,outputPath="", framespersecond=30, outputName = "", video = ""):
-    if(video!=""):
-        my_clip = mp.VideoFileClip(video)
-        framespersecond = my_clip.fps
-        my_clip.audio.write_audiofile("tempExtactedAudio.wav")
-    try:
-        os.mkdir(outputPath)
-    except:
-        pass
-    if(outputName==""):
-        outputName= framesPath.split(os.sep)[-1]
-    cwd = os.getcwd()
-    files = onlyfiles(framesPath)
-    for f in files:
-        name = f.split(os.sep)[-1].split(".")[0]
-        padded = name.zfill(11)
-        extension = f.split(".")[-1]
-        directory = os.sep.join(f.rsplit(os.sep)[0:-1])
-        #print(directory +os.sep+ padded + "."+extension)
-        os.rename(f,directory + os.sep+  padded +"."+ extension )
-    command = "ffmpeg -framerate "+ str(framespersecond)+" -thread_queue_size 1024 -i "
-    command +=  framesPath + os.sep + '%011d.' + extension+ ' ' +cwd+os.sep +outputPath+ os.sep+ outputName  +".mp4"
-    if(video!=""):
-        command += " -i tempExtactedAudio.wav"
-
-    print(command)
-    os.system(command)
-    if(video!=""):
-        os.remove("tempExtactedAudio.wav")
-
-extractFrames("NzishIREebw.webm", "testt")
-renderFromFrames("testt",video = "NzishIREebw.webm")#, video = "C1zuQ4AgACU.webm")
