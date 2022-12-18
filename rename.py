6794b4a994602ffd456741d4ed106bc675d13f9d
oh-hi-marx <flabdula@gmail.com>
2022-12-18 15:17:40 +1300

    zfil

---
+++ rename.py
@@ -0,0 +1,20 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import shutil
+import re
+
+
+def onlyfiles(mypath):
+	onlyfiles = [join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f))]
+	return onlyfiles
+
+files= onlyfiles("./")
+print(len(files))
+
+for file in files:
+    f= file.split(".")[0]
+    ext = file.split(".")[-1]
+    f = file.zfill(11) + "." + ext
+    print(file,f)
+    #os.rename(file, f)
