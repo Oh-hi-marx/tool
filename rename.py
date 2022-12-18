0db941937609844340bbae229b4b68a944ea06e8
oh-hi-marx <flabdula@gmail.com>
2022-12-18 15:37:16 +1300

    auto commit -working

--- rename.py
+++ rename.py
@@ -8,7 +8,7 @@ import re
 def onlyfiles(mypath):
 	onlyfiles = [join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f))]
 	return onlyfiles
-path = input("path")
+path = input("path: ")
 if(path == ""):
 	path = "./"
 
@@ -16,8 +16,9 @@ files= onlyfiles(path)
 print(len(files))
 
 for file in files:
-    f= file.split(".")[0]
-    ext = file.split(".")[-1]
-    f = file.zfill(11) + "." + ext
-    print(file,f)
-    #os.rename(file, f)
+	base = file.split(os.sep)[0]
+	f= file.split(".")[0].split(os.sep)[-1]
+	ext = file.split(".")[-1]
+	final = base+ os.sep + f.zfill(11) + "." + ext
+	print(file,final)
+	os.rename(file, final)
